e933c6663dc45c932be1116bbdaad3c12e2822e5
ravigoku <ravi.bauer@rutgers.edu>
2017-12-30 19:41:56 -0500

    added credentials and authentication functions for reddit api

--- reddart.py
+++ reddart.py
@@ -1,2 +1,24 @@
 import praw
-praw.Reddit(client_id = )
+import regex as re
+
+def credentials(textfile):
+    f = open(textfile)
+    words = f.read()
+    credRegex= re.compile(r'username = ([a-zA-Z0-9._%+-]+)\npassword = ([a-zA-Z0-9._%+-]+)\nclient_id = ([a-zA-Z0-9._%+-]+)\nclient_secret = ([a-zA-Z0-9._%+-]+)')
+    result = credRegex.search(words)
+    
+    return {'username':result.group(1), 'password':result.group(2), 
+            'client_id':result.group(3), 'client_secret':result.group(4)}
+   
+def createReddit(dictionary):
+    reddit = praw.Reddit(client_id = dictionary['client_id'],
+            client_secret = dictionary['client_secret'],
+            password = dictionary['password'],
+            user_agent = 'WallArt by /u/ravigoku',
+            username = dictionary['username'])
+    
+    print reddit.user    
+
+auth0 = credentials('C:\\Users\\inzon_000\\Documents\\python\\apiSecrets\\wallart.txt.')
+
+createReddit(auth0)
